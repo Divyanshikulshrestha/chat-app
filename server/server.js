8b92c6386007624971f18531c4a6ac41bc3e675d
divyanshi kulshrestha <divyanshikulshrestha9@gmail.com>
2025-08-24 15:35:24 +0530

    last commit

--- server/server.js
+++ server/server.js
@@ -63,5 +63,10 @@ await connectDB();
 // ===============================
 // Start Server
 // ===============================
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+}
+
+// Export server for Vercel
+export default server
